54a87e9cbb59a7f5be7c97ae02c7688b45b86b84
kevin Martínez <kevinccbsg@gmail.com>
2020-02-29 14:24:34 +0100

    Add log system to the example app

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const { join } = require('path');
+const fs = require('fs');
 const helmet = require('helmet');
 const compression = require('compression');
 const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
 
+const isProd = process.env.NODE_ENV === 'production';
+
+let logRoute = isProd ? join('/', 'var', 'log', 'node-app.log') : join(__dirname, 'node-app.log');
+
+const accessLogStream = fs.createWriteStream(logRoute, {
+  flags: 'a',
+});
+
 const app = express();
 
 app.use(helmet());
 app.use(compression());
-app.use(morgan('combined'));
+app.use(morgan('combined', { stream: accessLogStream }));
 
 app.use(express.static(join(__dirname, 'public')));
 
